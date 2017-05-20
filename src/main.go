0abd36b243c89fd26e0a202d6a916b4d9d866e37
DJviolin <kerozin.joe@gmail.com>
2017-05-20 21:24:02 +0200

    2017. 05. 20. 21:24:02,22

--- src/main.go
+++ src/main.go
@@ -8,8 +8,14 @@ import (
 )
 
 func main() {
+	// Static files
+	// http://stackoverflow.com/a/26563418/1442219
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
+
+	// Routes
 	http.HandleFunc("/", controllers.Index)
 
+	// Server
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
