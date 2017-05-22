011b1692de75622ffd0b8bbf106ba5473e3d098f
DJviolin <kerozin.joe@gmail.com>
2017-05-22 19:28:06 +0200

    2017. 05. 22. 19:28:05,96

--- src/lib/env.go
+++ src/lib/env.go
@@ -17,7 +17,7 @@ var User = os.Getenv("PGUSER")
 
 // Port : returns the port from env or string
 func Port() int {
-	// Atoi, which returns an error as the second return value
+	// Atoi converts to int, which returns an error as the second return value
 	port, err := strconv.Atoi(os.Getenv("PORT"))
 	if err != nil {
 		log.Print("Unable to get PORT env, falling back to :8080")
